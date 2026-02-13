c3661f8f48e349dafaddb4ff28d26b6c1a379ccd
soulware1 <slop@example.com>
2026-02-12 21:29:33 -0600

    scale fix

--- localization/en-us.lua
+++ localization/en-us.lua
@@ -1517,6 +1517,8 @@ return {
             k_MDJ_left_parenthisis = "(",
             k_MDJ_right_parenthisis = ")",
             k_MDJ_currently = "Currently",
+        },
+        v_dictionary = {
             k_MDJ_scaling_xmult = "+#1# XMult",
             k_MDJ_scaling_chips = "+#1# Chips",
             k_MDJ_scaling_xchips = "+#1# XChips",
